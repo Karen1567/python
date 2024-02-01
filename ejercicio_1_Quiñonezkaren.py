d34c3123c562a21f0aa0c9e5cd05dfffcb11508f
Karen <karendanielaquinonezvillamizar@gmail.com>
2024-02-01 14:02:06 -0500

    koko

--- ejercicio_1_Quiñonezkaren.py
+++ ejercicio_1_Quiñonezkaren.py
@@ -1,8 +1,3 @@
-#REALIZAR UN PROGRAMA QUE USE UN DICCIONARIO PARA GESTIONAR LOS PRODUCTOS
-#Y PRECIOS DE LA TABLA, DONDE SE LE PREGUNTE AL USUARIO POR UN PRODUCTO
-#Y LA CANTIDAD. AL FINALIZAR MOSTRAR EN LA CONSOLA EL PRECIO TOTAL. SI
-#EL PRODUCTO NO ESTA DEBE MOSTRAR UN MENSAJE INFORMANDO SOBRE ELLO
-
 diccionario = {"arroz":2500,"leche":3000,"juguitodecaja":5000}
 for llave,valor in diccionario.items():
     print (llave,valor)
@@ -11,15 +6,15 @@ if a=="arroz":
     c=int(input(""))
     b=2500*c
     print(b)
-    if a=="leche":
-        c=int(input(""))
-        b=3000*c
-        print(b)
-        if a=="juguitodecaja":
-            c=int(input(""))
-            b=5000*c
-            print(b)
-        else:
-            print("el producto no existe")
+elif a=="leche":
+    c=int(input(""))
+    b=3000*c
+    print(b)
+elif a=="juguitodecaja":
+    c=int(input(""))
+    b=5000*c
+    print(b)
+else:
+    print("el producto no existe")
             
     
