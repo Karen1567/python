04ef2c4d39613d074659a72685655aecfed47f0e
Karen <karendanielaquinonezvillamizar@gmail.com>
2024-02-07 09:15:20 -0500

    noshe

---
+++ Dia 8/ejercicio_1.py
@@ -0,0 +1,10 @@
+#1. Devuelve un listado con todos los pedidos que se han realizado. 
+#Los pedidos deben estar ordenados por la fecha de realización,
+# mostrando en primer lugar los pedidos más recientes.
+import json
+
+x= open('datos.json')
+miJson = json.load(x)
+print(miJson)
+
+
